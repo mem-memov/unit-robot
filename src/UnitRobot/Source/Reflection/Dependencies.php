226d3f587e00c951c4ee8ac39d3267aa24d22b3c
Alexander A. Novikov <mem-memov@yandex.ru>
2017-08-22 15:34:03 +0000

    unused code removed

--- src/UnitRobot/Source/Reflection/Dependencies.php
+++ src/UnitRobot/Source/Reflection/Dependencies.php
@@ -22,21 +22,7 @@ class Dependencies
         $this->descriptionDependencies = $descriptionDependencies;
         $this->instancies = $instancies;
     }
-    
-    public function addDependenciesToUnitTest(
-        Text $sourceText, 
-        UnitTest $unitTest
-    ): void
-    {
-        $prelude = $sourceText->extract(1, $this->class->getStartLine() - 1);
-        
-        preg_match_all('/(use .+;)/', $prelude, $matches);
-        
-        foreach ($matches[1] as $useStatement) {
-            $unitTest->addDependency($useStatement);
-        }
-    }
-    
+
     public function describe(
         Text $sourceText
     ): InstanceDependencies
