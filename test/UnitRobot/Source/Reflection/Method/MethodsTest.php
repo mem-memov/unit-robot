3795c3f42a677f32a3360012d55afa9a48f05cdd
Alexander A. Novikov <mem-memov@yandex.ru>
2017-08-23 08:38:16 +0000

    a fix

--- test/UnitRobot/Source/Reflection/Method/MethodsTest.php
+++ test/UnitRobot/Source/Reflection/Method/MethodsTest.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace MemMemov\UnitRobot\Source\Reflection\Method;
 
+use MemMemov\UnitRobot\Source\Reflection\Comment\MethodComments;
 use MemMemov\UnitRobot\Source\Reflection\Method\Call\Calls;
+use MemMemov\UnitRobot\Source\Reflection\Method\ReturnType\ReturnTypes;
 use MemMemov\UnitRobot\Source\Reflection\Parameter\Parameters;
 use MemMemov\UnitRobot\Source\Token\MethodSignatures as MethodSignatureTokens;
 use MemMemov\UnitRobot\Source\Token\MethodBodies as MethodBodyTokens;
-use MemMemov\UnitRobot\Source\Reflection\Comment\MethodComments;
 use MemMemov\UnitRobot\Source\Description\Signature\Signatures;
-use MemMemov\UnitRobot\Source\Reflection\Method\ReturnType\ReturnTypes;
 use PHPUnit\Framework\TestCase;
 
 final class MethodsTest extends TestCase
